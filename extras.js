9327a8d28fa528b48091e2b009b4e8def27fede4
ccardoso97 <ccardoso210@gmail.com>
2022-01-19 19:06:26 -0300

    Projeto1

---
+++ extras.js
@@ -0,0 +1,28 @@
+const prompt = require("prompt-sync")();
+//1
+
+// console.log('O primeiro programa a gente nunca esquece!')
+
+
+//2 
+
+// console.log(`
+// Nome: Caio Luiz Cardoso Oliveira
+// Endereço: Rua Santa Clara, 95, Costa Carvalho
+// CEP: 36070-120
+// Tel.: 32 9 91394691
+// `);
+
+//3
+
+console.log(`
+Eu tinha uma galinha; 
+Que se chamava Marylou;
+Um dia fiquei com fome;
+E papei a Marylou; 
+Marylou, Marylou;
+Tinha cara de babaca;
+Marylou, Marylou;
+Botava ovo pela cloaca...
+`);
+
---
+++ revisao.js
@@ -0,0 +1,32 @@
+
+const prompt = require("prompt-sync")();
+
+// // EX 03 
+
+// let anos = +prompt("Digite os anos: ")
+// let meses = +prompt("Digite os meses: ")
+// let dias = +prompt("Digite os dias: ")
+
+// let anosparadias = anos * 365;
+// let mesesparadias = meses *30;
+// let totalDias = anosparadias + mesesparadias + dias;
+
+// console.log("Total de dias que você viveu: " + totalDias);
+
+// // EX 04
+
+// let idadeEmDias = +prompt("Digite sua idade em dias: ");
+
+// console.log(idadeEmDias);
+
+// let diasEmAnos = Math.trunc(idadeEmDias / 365);
+// console.log("Idade em anos: " + diasEmAnos);
+
+// let restoDias1 = idadeEmDias % 365;
+
+// let diasEmMeses = Math.trunc(restoDias1 / 30);
+// console.log("Idade em meses: " + diasEmMeses);
+
+// let restoDias2 = restoDias1 % 30;
+// console.log(`Idade em dias: ${restoDias2}`);
+
