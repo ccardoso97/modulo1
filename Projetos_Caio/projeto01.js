3cc38047fe8a3cbfdad866a38df30dab77ded6dc
ccardoso97 <ccardoso210@gmail.com>
2022-01-21 23:00:39 -0300

    Projeto finalizado

--- Projetos_Caio/projeto01.js
+++ Projetos_Caio/projeto01.js
@@ -1,7 +1,10 @@
 const prompt = require("prompt-sync")();
 
-let nome = prompt(`Escolha um nome para o seu personagem: `);
-console.log(` 
+var repetir = "sim";
+while (repetir == "sim") {
+  console.clear();
+  let nome = prompt(`Escolha um nome para o seu personagem: `);
+  console.log(` 
 ${nome}, um fiscal da vigilância sanitária  
 teve um grande problema,
 algumas pessoas de sua cidade estavam ficando doentes 
@@ -12,36 +15,120 @@ com cinco critérios avaliativos
 pensados para classificar os estabelecimentos.
 Ao fim da avaliação, ele poderia determinar se 
 o estalecimento é: Ótimo, Bom, Regular, Ruim ou Péssimo.
-Responda às seguintes perguntas para ser avaliado:
-`);
-let estabelecimento = prompt(`Qual o nome do açougue? `);
-console.log ();
-let higienePessoal = prompt(`Todos os funcionários de ${estabelecimento} estão com as mãos limpas? `).toLowerCase();
-let limpezaDaLoja = prompt('O chão, balcão e geladeiras foram higienizados corretamente? ').toLowerCase();
-let ferramentas = prompt('Todas as ferramentas estão afiadas? ').toLowerCase();
-let seguranca = prompt('Os uniformes, toucas e luvas estão dentro dos padrões? ').toLowerCase();
-let produtos = prompt('Os produtos estão todos dentro da validade e armazenados de maneira correta? ').toLowerCase();
+Responda às seguintes perguntas para ser avaliado:`);
+  console.log("-------------------------------------------------------");
+  let estabelecimento = prompt(`Qual o nome do açougue? `);
+  console.log();
+  let higienePessoal = prompt(
+    `Todos os funcionários de ${estabelecimento} estão com as mãos limpas? `
+  ).toLowerCase();
+  while (higienePessoal !== "nao" && higienePessoal !== "sim") {
+    console.log("Responda com 'Sim' ou 'Não'");
+    higienePessoal = prompt(
+      `Todos os funcionários de ${estabelecimento} estão com as mãos limpas? `
+    ).toLowerCase();
+  }
+  let limpezaDaLoja = prompt(
+    "O chão, balcão e geladeiras foram higienizados corretamente? "
+  ).toLowerCase();
+  while (limpezaDaLoja !== "nao" && limpezaDaLoja !== "sim") {
+    console.log("Responda com 'Sim' ou 'Não'");
+    limpezaDaLoja = prompt(
+      "O chão, balcão e geladeiras foram higienizados corretamente? "
+    ).toLowerCase();
+  }
+  let ferramentas = prompt(
+    "Todas as ferramentas estão afiadas? "
+  ).toLowerCase();
+  while (ferramentas !== "nao" && ferramentas !== "sim") {
+    console.log("Responda com 'Sim' ou 'Não'");
+    ferramentas = prompt("Todas as ferramentas estão afiadas? ").toLowerCase();
+  }
+  let seguranca = prompt(
+    "Os uniformes, toucas e luvas estão dentro dos padrões? "
+  ).toLowerCase();
+  while (seguranca !== "nao" && seguranca !== "sim") {
+    console.log("Responda com 'Sim' ou 'Não'");
+    seguranca = prompt(
+      "Os uniformes, toucas e luvas estão dentro dos padrões? "
+    ).toLowerCase();
+  }
+  let produtos = prompt(
+    "Os produtos estão todos dentro da validade e armazenados de maneira correta? "
+  ).toLowerCase();
+  while (produtos !== "nao" && produtos !== "sim") {
+    console.log("Responda com 'Sim' ou 'Não'");
+    produtos = prompt(
+      "Os produtos estão todos dentro da validade e armazenados de maneira correta? "
+    ).toLowerCase();
+  }
 
-let respostas = 0 
-if (higienePessoal == 'sim'){respostas++};
-if (limpezaDaLoja == 'sim'){respostas++};
-if (ferramentas == 'sim'){respostas++};
-if (seguranca == 'sim'){respostas++};
-if (produtos == 'sim'){respostas++};
+  let respostasSim = 0;
+  let respostasNao = 0;
+  if (higienePessoal == "sim") {
+    respostasSim++;
+  }
+  if (higienePessoal == "nao") {
+    respostasNao++;
+  }
+  if (limpezaDaLoja == "sim") {
+    respostasSim++;
+  }
+  if (limpezaDaLoja == "nao") {
+    respostasNao++;
+  }
+  if (ferramentas == "sim") {
+    respostasSim++;
+  }
+  if (ferramentas == "nao") {
+    respostasNao++;
+  }
+  if (seguranca == "sim") {
+    respostasSim++;
+  }
+  if (seguranca == "nao") {
+    respostasNao++;
+  }
+  if (produtos == "sim") {
+    respostasSim++;
+  }
+  if (produtos == "nao") {
+    respostasNao++;
+  }
+  console.log("-------------------------------------------------------");
 
-console.log(`A quantidade de respostas 'Sim' foi: ${respostas}.`);
-if (respostas == 0){
-    console.log(`Péssimo! O ${estabelecimento} será interditado imediatamente!`);
-} else if (respostas == 1 && 2){
-    console.log(`Ruim! O ${estabelecimento} será multado e deve se adequar as regras sanitárias!`);
-} else if (respostas == 3){
-    console.log(`Regular! O ${estabelecimento} passará por uma vistoria técnica e deverá se readequar!`);
-} else if (respostas == 4){
+  console.log(`Critério(s) aprovado(s): ${respostasSim}.`);
+  if (respostasNao !== 0) {
+    console.log(`Critério(s) reprovado(s): ${respostasNao}.`);
+  }
+
+  console.log("-------------------------------------------------------");
+
+  if (respostasSim == 0) {
+    console.log(
+      `Péssimo! O ${estabelecimento} será interditado imediatamente!`
+    );
+  } else if (respostasSim == 1 && 2) {
+    console.log(
+      `Ruim! O ${estabelecimento} será multado e deve se adequar as regras sanitárias!`
+    );
+  } else if (respostasSim == 3) {
+    console.log(
+      `Regular! O ${estabelecimento} passará por uma vistoria técnica e deverá se readequar!`
+    );
+  } else if (respostasSim == 4) {
     console.log(`Bom! O ${estabelecimento} está de acordo com as normas, sendo necessárias 
     apenas algumas correções!`);
-} else if (respostas == 5){
-    console.log(`Ótimo! O ${estabelecimento} é um exemplo entre os açougues e receberá um certificado 
-de honra ao mérito!`);
-} console.log ();
-console.log(`Fim da avaliação de ${estabelecimento}.`);
+  } else if (respostasSim == 5) {
+    console.log(`Ótimo! O ${estabelecimento} é um exemplo entre os açougues 
+e receberá um certificado de honra ao mérito!`);
+  }
+  console.log("-------------------------------------------------------");
+  console.log(`Fim da avaliação de ${estabelecimento}.`);
+  console.log("-------------------------------------------------------");
+
+  repetir = prompt(
+    "Você deseja avaliar um novo estabelecimento? "
+  ).toLowerCase();
+}
 console.log();
