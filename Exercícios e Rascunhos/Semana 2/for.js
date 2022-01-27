085ac8ca9d822a1780f3533ba0df1380013284c8
ccardoso97 <ccardoso210@gmail.com>
2022-01-26 22:55:19 -0300

    for

--- Exercícios e Rascunhos/Semana 2/for.js
+++ Exercícios e Rascunhos/Semana 2/for.js
@@ -1,4 +1,12 @@
+const prompt = require('prompt-sync')();
 
-for (let i = 0; i < 10; i++) {
-    console.log("Isso é uma repetição!");
+//EXERCICIO 3
+
+let num = +prompt('Digite um numero para ter a sua tabuada: ')
+
+for(let i = 1; i <= 10; i++){ //i é o contador. 10 é a quantidade 
+    // de repetições. i++ adiociona +1 ao i a 
+    //cada repetição e faz ele parar no 10.
+    console.log(`${num} * ${i} = ${num*i}`) 
 }
+//
